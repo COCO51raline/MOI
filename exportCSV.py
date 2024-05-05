086b1130566f20745bb942c15cbca25ab8ed08b4
Alexandre <tgvalex@hotmail.fr>
2024-05-05 17:18:47 +0200

    feat: create export csv function

--- exportCSV.py
+++ exportCSV.py
@@ -1,5 +1,9 @@
-def expotInventaire (inventaire):
-    fichierAecraser=open(inventaire,'w')
+def exportInventaire (inventaire,path= 'Export.csv'):
+    fichierAecraser=open(path,'w')
+    fichierAecraser.write('Ingrédients;Quantité;Prix;Minimale;Péremption\n')
+    for clé, valeurs in inventaire.items():
+        fichierAecraser.write(f'{clé};{valeurs[0]};{valeurs[1]};{valeurs[3]};{valeurs[2]}\n')
+    fichierAecraser.close()
 
 
-if __name__ == '__main__':
+
